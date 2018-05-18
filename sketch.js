231780dd6c5d03a6067ec9bbdad4c91bf7e7e025
theodormarcu <tmarcu@princeton.edu>
2018-05-18 16:44:32 -0400

    Image setup. Removed pause screen, users thought it was a glitch. Theodor

--- sketch.js
+++ sketch.js
@@ -15,18 +15,18 @@ tmarcu@princeton.edu
 var flag = 0;
 let creditText = "by Theodor Marcu \ntheodormarcu.com";
 
-function setup() { 
+function setup() {
   createCanvas(1000, 1000);
   textSize(24);
   textFont('Helvetica');
-} 
+}
 
-function draw() { 
+function draw() {
   // Background
   background('#e74c3c');
   fill('white');
   textAlign(RIGHT);
-  text(creditText, 975, 900);  
+  text(creditText, 975, 900);
   // Time vars
   let h = hour();
   let m = minute();
@@ -40,7 +40,7 @@ function draw() {
   let circleWidth = 600;
   let circleHeight = 600;
   ellipse(xCenter, yCenter, circleWidth, circleHeight);
-  
+
   // Math part behind clock
   angleMode(DEGREES);
   let hourWidth = 300;
@@ -52,7 +52,7 @@ function draw() {
   // Create Hour "Hand"
   fill('#e74c3c');
   ellipse(xH, yH, hourWidth, hourWidth);
-            
+
   // Create Minute "Hand"
 	let minuteWidth = 100
   let minuteCircleRadius = hourWidth / 2 - 50;
@@ -61,7 +61,7 @@ function draw() {
   let yM = yH + minuteCircleRadius * cos(thetaM);
   fill('white');
   ellipse(xM, yM, minuteWidth, minuteWidth);
-  
+
   // Create Second "Hand"
   let secondWidth = 25
   let secondCircleRadius = minuteWidth / 2 - 12.5;
@@ -73,10 +73,10 @@ function draw() {
 
 	// Create stop if mouse is moving
   if (mouseY !== pmouseY || mouseX !== pmouseX) {
-    fill('#8e44ad');
-  	rect(0, 0, 1000, 1000);
-    fill('white');
-    textAlign(CENTER);
-    text("You have to stop moving \n to understand time.", 500, 500);
+   //  fill('#8e44ad');
+  	// rect(0, 0, 1000, 1000);
+   //  fill('white');
+   //  textAlign(CENTER);
+   //  text("You have to stop moving \n to understand time.", 500, 500);
   }
 }
